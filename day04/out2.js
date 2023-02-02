362887815525973034fc6e03c17ee69ec85abf46
aliciawill100 <124111025+aliciawill100@users.noreply.github.com>
2023-02-02 17:34:42 +0900

    day04프로젝트

---
+++ day04/out2.js
@@ -0,0 +1,10 @@
+function 여행하다() {
+    console.log("1. 돈을 마련한다")
+    console.log("2. 교통편과 숙박장소 예매한다")
+    console.log("3. 여행에서 계획대로 즐긴다")
+}
+function 쉬다(){
+    console.log("1. 눕는다")
+    console.log("2. 눈을 감는다")
+    console.log("3. 명상한다")
+}
